395c223bbb1ceda8533f72bf1ea1841648d6f68e
Sterben <142906656+HSterben@users.noreply.github.com>
2023-11-16 13:33:13 -0500

    Update FooBar.java
    commit foobar

--- src/main/java/com/prog2/labs/FooBar.java
+++ src/main/java/com/prog2/labs/FooBar.java
@@ -1,32 +1,40 @@
 package com.prog2.labs;
 
+import java.util.concurrent.Semaphore;
+
 /**
  * @author adinashby
  *
  */
-
 class FooBar {
+
     private int n;
+    private Semaphore sem1;
+    private Semaphore sem2;
 
     public FooBar(int n) {
         this.n = n;
+        sem1 = new Semaphore(1);
+        sem2 = new Semaphore(0);
     }
 
     public void foo(Runnable printFoo) throws InterruptedException {
-        
+
         for (int i = 0; i < n; i++) {
-            
-        	// printFoo.run() outputs "foo". Do not change or remove this line.
-        	printFoo.run();
+            sem1.acquireUninterruptibly();
+            // printFoo.run() outputs "foo". Do not change or remove this line.
+            printFoo.run();
+            sem2.release();
         }
     }
 
     public void bar(Runnable printBar) throws InterruptedException {
-        
+
         for (int i = 0; i < n; i++) {
-            
+            sem2.acquireUninterruptibly();
             // printBar.run() outputs "bar". Do not change or remove this line.
-        	printBar.run();
+            printBar.run();
+            sem1.release();
         }
     }
 }
